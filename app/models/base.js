29e45bbb3b10b76caf43c025cca60957cfad1033
jonez <jdown@eastlink.ca>
2013-09-02 15:14:00 -0300

    restructure app

--- app/models/base.js
+++ app/models/base.js
@@ -1,15 +1,16 @@
+
 module.exports = function( db ) {
 	this.db = db;
 };
 
 module.exports.prototype = {
-	extend: function( options ) {
+	extend: function( properties ) {
 		var child = module.exports;
 
 		child.prototype = module.exports.prototype;
 
-		for ( var key in options ) {
-			child.prototype[ key ] = options[ key ];
+		for ( var key in properties ) {
+			child.prototype[ key ] = properties[ key ];
 		}
 
 		return child;
