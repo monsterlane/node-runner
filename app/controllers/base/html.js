3fd0d073034122284eb9ba2fd415aa7ec96d3a83
jonez <jdown@eastlink.ca>
2013-09-18 20:28:29 -0300

    stub out html view

--- app/controllers/base/html.js
+++ app/controllers/base/html.js
@@ -4,8 +4,10 @@
  */
 
 var util = require( 'util' ),
+	fs = require( 'fs' ),
 	async = require( 'async' ),
-	Base_view = require( './base' );
+	dot = require( 'dot' ),
+	Base_view = require( './view' );
 
 function Html_view( ) {
 	Base_view.apply( this, arguments );
@@ -14,11 +16,55 @@ function Html_view( ) {
 util.inherits( Html_view, Base_view );
 
 /**
+ * Method: template
+ */
+
+Html_view.prototype.template = function( path, def, callback ) {
+	var def = def || { };
+
+	fs.readFile( process.argv[ 1 ].replace( /\/[^\/]*$/, path ), 'utf8', function( err, data ) {
+		var tpl, str;
+
+		if ( err ) {
+			callback( new Error( err ), null );
+		}
+		else {
+			tpl = dot.compile( data );
+			str = tpl( def );
+
+			callback( null, str );
+		}
+	});
+};
+
+/**
  * Method: render
  */
 
-Html_view.prototype.render = function( ) {
+Html_view.prototype.render = function(  ) {
+	var body = body || '',
+		self = this;
 
+	async.parallel([
+		function( callback ) {
+			self._getDocumentHeader( { }, function( err, content ) {
+				callback( null, content );
+			});
+		},
+		function( callback ) {
+			self._getDocumentFooter( { }, function( err, content ) {
+				callback( null, content );
+			});
+		}
+	], function( err, results ) {
+		if ( err ) throw Error( err );
+
+		self._response.render( __dirname + '/../base/views/document', {
+			header: results[ 0 ],
+			body: body,
+			footer: results[ 1 ]
+		});
+	});
 };
 
 /* bind */
